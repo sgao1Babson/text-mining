346b34a6816e5c8f8135a5baef8fa949aa244f2a
sgao1Babson <sgao1@babson.edu>
2022-10-31 19:01:30 -0400

    Update part_1_code.py

--- part_1_code.py
+++ part_1_code.py
@@ -2,13 +2,13 @@ import urllib.request
 
 hamlet_url = 'https://www.gutenberg.org/files/2265/2265.txt'
 response = urllib.request.urlopen(hamlet_url)
-data = response.read()  # a `bytes` object
+data = response.read() 
 text = data.decode('utf-8')
-print(text) # for testing
+print(text)
 
 macbeth_url = 'https://www.gutenberg.org/files/2264/2264.txt'
-response = urllib.request.urlopen(hamlet_url)
-data = response.read()  # a `bytes` object
+response = urllib.request.urlopen(macbeth_url)
+data = response.read()  
 text = data.decode('utf-8')
 print(text) # for testing
 
